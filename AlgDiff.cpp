5727bfdf998ad295abbd23d5adde609be8a2e160
Berke <berke.erikan@gmail.com>
2023-11-19 16:35:35 +0100

    Only go for midpoint or trapezoidal?

--- AlgDiff.cpp
+++ AlgDiff.cpp
@@ -1,28 +1,51 @@
 #include "AlgDiff.h"
-
-AlgDiff::AlgDiff(float ts, float alpha,float beta,int N,float T,float wc, bool corr)
-        :__ts {ts}, __alpha{alpha}, __beta{beta}, __N {N}, correction {corr}, __wc {wc}
+#include <iostream>
+AlgDiff::AlgDiff(float ts, float alpha, float beta, int N, float T, float wc, bool corr)
+    : __ts{ts}, __alpha{alpha}, __beta{beta}, __N{N}, correction{corr}, __wc{wc}
+{
+    if (0 == __N)
     {
-        if(0==__N){
-            __theta=0;
-            __thetaBool=false;
-        }
-        else{
-            //Calculate roots of jacobi here later and get max value as theta.
-            __thetaBool=true;
-        }
-
-        //Get corner frequency if window length is given. Get window length if corner frequency is given.
-
-        if (T){
-            __T=T;
-            __wc=get_cutoffFreq();
-            __setUp='T';
-        }
-
+        __theta = 0;
+        __thetaBool = false;
     }
+    else
+    {
+        // Calculate roots of jacobi here later and get max value as theta.
+        __thetaBool = true;
+    }
+
+    // Get corner frequency if window length is given. Get window length if corner frequency is given.
+
+    if (T)
+    {
+        __T = T;
+        __wc = get_cutoffFreq();
+        __setUp = 'T';
+    }
+    else if (wc)
+    {
+        __wc = wc;
+        computeTfromWc(__wc);
+        __setUp = 'w';
+    }
+    else
+        throw std::invalid_argument("T or wc must be given."); // Maybe unnecessary to check?
+}
+
+float AlgDiff::get_cutoffFreq(void)
+{
+    // Calculate wc based on T here.
+    return 0;
+}
+void AlgDiff::computeTfromWc(float wc)
+{
+    // Calculate T based on wc here.
+    return;
+}
+
+void AlgDiff::discretize(int der, bool reduceFilLength, float redTol, bool discreteSpectrum,method mtd)
+{
+    int L0=static_cast<int>(__T/__ts);
     
-    float get_cutoffFreq(void){
-        //Calculate wc based on T here.
-        return 0;
-    }
+
+}
--- AlgDiff.h
+++ AlgDiff.h
@@ -1,5 +1,20 @@
+#pragma once
+/*
+#define MID_POINT 0
+#define EULER 1
+#define TRAPEZOIDAL 2
+#define SIMPSON_RULE 3
+#define SIMPSON_38_RULE 4
+#define BOOLE_RULE 5
+*/
+enum method {mid_point,euler,trapezoidal,simpson_rule,simpson_38_rule,boole_rule};
+
+
 class AlgDiff{
     AlgDiff::AlgDiff(float ts, float alpha,float beta,int N,float T,float wc, bool corr);
+    float get_cutoffFreq(void);
+    void computeTfromWc(float wc);
+    void discretize(int der, bool reduceFilLength, float redTol, bool discreteSpectrum,method mtd);
 private:
     float __ts,__alpha,__beta,__T,__theta,__wc;
     int __N;
